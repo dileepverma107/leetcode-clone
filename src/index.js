d8eda87d4fcec32a84c4e081ba14ac08413db419
Dileep Verma <dileepverma107@gmail.com>
2024-08-07 17:13:28 +0530

    Update clerk key

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import reportWebVitals from './reportWebVitals';
 import { ClerkProvider } from '@clerk/clerk-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const PUBLISHABLE_KEY = "pk_test_ZGVjaWRpbmctYWtpdGEtNDEuY2xlcmsuYWNjb3VudHMuZGV2JA";
-//const PUBLISHABLE_KEY = process.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+const PUBLISHABLE_KEY = process.env.VITE_CLERK_PUBLISHABLE_KEY;
 if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key")
 }
